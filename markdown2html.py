33158203d56360479fffb07ca679dc4ed12c2a6e
MisturaO <misturaojulari91@gmail.com>
2023-10-16 04:43:26 +0100

    Checks for the num of args passed to the script

--- markdown2html.py
+++ markdown2html.py
@@ -2,7 +2,7 @@
 """
 This script converts a markdown file to HTML. It takes a argument(a string i.e. the markdown file)
     Usage: 
-        ./markdown2html.py README.md
+        Usage: ./markdown2html.py README.md README.html
 """
 
 from sys import argv
@@ -13,13 +13,13 @@ import os
 if __name__ == "__main__":
 
     if len(argv) != 3:
-        sys.stderr.write('Usage: ./markdown2html.py README.md README.html \n')
+        sys.stderr.write('Usage: ./markdown2html.py README.md README.html')
         sys.exit(1)
 
     markdown_file = argv[1]
 
     if not os.path.exists(markdown_file):
-        sys.stderr.write(f"Missing {markdown_file}\n")
+        sys.stderr.write(f"Missing {markdown_file}")
         sys.exit(1)
 
     sys.exit(0)
