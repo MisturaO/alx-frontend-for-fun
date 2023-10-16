3f125d1aebc7bc6651c2efe45ae6abb0252b52a6
MisturaO <misturaojulari91@gmail.com>
2023-10-16 04:57:51 +0100

    Checks for the num of args passed to the script

--- markdown2html.py
+++ markdown2html.py
@@ -13,13 +13,13 @@ import os
 if __name__ == "__main__":
 
     if len(argv) != 3:
-        sys.stderr.write('Usage: ./markdown2html.py README.md README.html')
+        sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
         sys.exit(1)
 
     markdown_file = argv[1]
 
     if not os.path.exists(markdown_file):
-        sys.stderr.write(f"Missing {markdown_file}")
+        sys.stderr.write(f"Missing {markdown_file}\n")
         sys.exit(1)
     
     sys.exit(0)
